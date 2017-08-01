c9084611dbe5408bc941bbf02ed2778e046b95d4
TranMacTien <tientranmac96@gmail.com>
2017-08-01 15:49:06 +0700

    init

---
+++ webcompany/js/myJS.js
@@ -0,0 +1,33 @@
+(function(){
+	var slideIndex=0;
+	showSlide(slideIndex);
+	document.getElementById("prev").addEventListener("click", function(){
+		showSlide(slideIndex-=1);
+	});
+	document.getElementById("next").addEventListener("click", function(){
+		showSlide(slideIndex+=1);
+	});
+
+	function showSlide(n){
+		var i;
+		var j;
+		var slides=document.getElementsByClassName("slide-img");
+		if(n>slides.length-1){
+			slideIndex=0;
+		}
+		if(n<0){
+			slideIndex=slides.length-1;
+		}
+		console.log(slideIndex);
+		for(i=0;i<slides.length;i++){
+			slides[i].style.display="none";
+		}
+		slides[slideIndex].style.display="block";
+		/*for(j=0;j<slides.length;j++){
+			if(j===slideIndex){
+				slides[j].style.display="block";
+			}
+		}	*/
+	}
+
+})();
