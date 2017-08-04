b408198d3e287c80012fb1d54f58f837bb33cc99
TranMacTien <tientranmac96@gmail.com>
2017-08-04 16:39:57 +0700

    update

--- webcompany/subpage/contact/myJS.js
+++ webcompany/subpage/contact/myJS.js
@@ -1,14 +1,14 @@
 function myMap() {
-	 var myLatLng = {lat: 10.782824, lng: 106.697488};
+	var myLatLng = {lat: 10.782824, lng: 106.697488};
 	var mapOptions = {
-	    center: myLatLng,
-	    zoom: 15,
-	    mapTypeId: google.maps.MapTypeId.ROADMAP
+		center: myLatLng,
+		zoom: 15,
+		mapTypeId: google.maps.MapTypeId.ROADMAP
 	}
-	var map = new google.maps.Map(document.getElementById("map"), mapOptions);
 	var marker = new google.maps.Marker({
-          position: myLatLng,
-          map: map,
-          title: 'My Company'
-        });
+		position: myLatLng,
+		map: map,
+		title: 'My Company'
+	});
+	var map = new google.maps.Map(document.getElementById("map"), mapOptions);
 }	
