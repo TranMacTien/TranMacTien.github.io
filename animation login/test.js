9d0e3d2641fb8a73f007e145f91cacb47af800e9
TranMacTien <tientranmac96@gmail.com>
2017-08-22 17:00:40 +0700

    format code

--- animation login/test.js
+++ animation login/test.js
@@ -1,16 +1,16 @@
-var btnSubmit=document.getElementById('btn-submit');
-btnSubmit.addEventListener('click', function(){
+var btnSubmit = document.getElementById('btn-submit');
+btnSubmit.addEventListener('click', function () {
     this.classList.add('login-form__btn--collapse');
-    setTimeout(function(){
+    setTimeout(function () {
         document.querySelector('.loading').classList.add('loading--show');
     }, 500);
-    document.querySelector('.login-form__btn>span').style.visibility='hidden';
-    setTimeout(function(){
+    document.querySelector('.login-form__btn>span').style.visibility = 'hidden';
+    setTimeout(function () {
         document.querySelector('.scale-out').classList.add('scale');
     }, 1000);
-    setTimeout(function(){
-        document.querySelector('.container').style.display='none';
-        document.querySelector('.hello').style.visibility='visible';
+    setTimeout(function () {
+        document.querySelector('.container').style.display = 'none';
+        document.querySelector('.hello').style.visibility = 'visible';
         document.querySelector('.hello h1').classList.add('scale-zoom-in');
     }, 1200);
 });
