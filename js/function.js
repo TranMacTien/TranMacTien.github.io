7e9e06efbb81c44ea83693ea200b4a56fb3e2cbf
tranmactien <tientranmac96@gmail.com>
2017-08-31 18:36:18 +0700

    done

--- js/function.js
+++ js/function.js
@@ -1,28 +1,30 @@
 var toggle = document.querySelector('.toggle-button');
-console.log(toggle);
 toggle.addEventListener('click', function () {
-/*     var isVisible = toggle.style.visibility;
-    if (isVisible === 'visible') {
-        toggle.style.visibility='hidden';
-    }else{
-        toggle.style.visibility='visible';
-    } */
     this.classList.toggle('toggle-button--close');
     document.querySelector('.navbar__group').classList.toggle('navbar__group--toggle');
 });
-/* var navBar = document.querySelector('.navbar');
+/* media query */
+var mq = window.matchMedia("screen and (min-width: 900px)");
+mq.addListener(function () {
+    if (this.matches) {
+        document.querySelector('.wrapper').classList.remove('clearfix');
+    } else {
+        document.querySelector('.wrapper').classList.add('clearfix');
+    }
+});
+var navBar = document.querySelector('.navbar');
 var navBarItem = document.querySelectorAll('.navbar__item');
 window.addEventListener('scroll', function () {
     var scroll = document.body.scrollTop;
     if (scroll > 20) {
         navBar.classList.add('navbar--collapse');
-        for (var i = 0, n = navBarItem.length; i < n; i++){
+        for (var i = 0, n = navBarItem.length; i < n; i++) {
             navBarItem[i].classList.add('navbar__item--collapse');
         }
     } else if (scroll <= 20) {
         navBar.classList.remove('navbar--collapse');
-        for (var i = 0, n = navBarItem.length; i < n; i++){
+        for (var i = 0, n = navBarItem.length; i < n; i++) {
             navBarItem[i].classList.remove('navbar__item--collapse');
         }
     }
-}); */
+});
