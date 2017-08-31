21abd9f59168c1549642801bfcb288b9292c05af
tranmactien <tientranmac96@gmail.com>
2017-08-31 15:08:02 +0700

    done desktop

---
+++ js/function.js
@@ -0,0 +1,16 @@
+var navBar = document.querySelector('.navbar');
+var navBarItem = document.querySelectorAll('.navbar__item');
+window.addEventListener('scroll', function () {
+    var scroll = document.body.scrollTop;
+    if (scroll > 20) {
+        navBar.classList.add('navbar--collapse');
+        for (var i = 0, n = navBarItem.length; i < n; i++){
+            navBarItem[i].classList.add('navbar__item--collapse');
+        }
+    } else if (scroll <= 20) {
+        navBar.classList.remove('navbar--collapse');
+        for (var i = 0, n = navBarItem.length; i < n; i++){
+            navBarItem[i].classList.remove('navbar__item--collapse');
+        }
+    }
+});
