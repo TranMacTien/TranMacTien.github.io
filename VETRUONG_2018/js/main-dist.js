556a31da9cd5e65bb7f18d5f7297bd038dc59e37
mactien <mactien.tran@ogilvy.com>
2018-01-25 21:58:38 +0700

    update js

--- VETRUONG_2018/js/main-dist.js
+++ VETRUONG_2018/js/main-dist.js
@@ -1,13 +1,33 @@
 "use strict";
 
-function VETRUONG3() {
-    this.username = document.getElementById("username");
-}
+var VETRUONG3 = {
+    username: document.getElementById("username"),
+    password: document.getElementById("password"),
+    loader: document.querySelector(".loader"),
+    btnSubmit: document.getElementById("OK"),
+    formWrapper: document.querySelector(".form-wrapper"),
+    exec: function() {
+        this.handleLoader(), this.registerEvent();
+    },
+    verify: function() {
+        "" === this.username.value ? alert("Please input your name!") : "" === this.password.value ? alert("Please input gift code!") : "tien" !== this.password.value ? alert("Wrong gift code!") : alert("Success");
+    },
+    registerEvent: function() {
+        var e = this;
+        this.formWrapper.addEventListener("keyup", function(t) {
+            t.preventDefault(), 13 === t.keyCode && e.btnSubmit.click();
+        }), this.btnSubmit.addEventListener("click", function() {
+            e.verify();
+        });
+    },
+    handleLoader: function() {
+        var e = this;
+        window.addEventListener("load", function() {
+            setTimeout(function() {
+                e.loader.classList.add("is-hidden");
+            }, 2e3);
+        });
+    }
+};
 
-window.addEventListener("load", function() {
-    setTimeout(function() {
-        document.querySelector(".loader").classList.add("is-hidden");
-    }, 2e3);
-});
-
-var vetruong = new VETRUONG3();
+VETRUONG3.exec.bind(VETRUONG3)();
--- VETRUONG_2018/js/main.js
+++ VETRUONG_2018/js/main.js
@@ -1,13 +1,43 @@
-window.addEventListener('load', () => {
-    setTimeout(() => {
-        document.querySelector('.loader').classList.add('is-hidden');
-    }, 2000)
-});
-
-let vetruong = new VETRUONG3();
-
-function VETRUONG3() {
-    this.username = document.getElementById('username');
-
-
+let VETRUONG3 = {
+    username: document.getElementById('username'),
+    password: document.getElementById('password'),
+    loader:  document.querySelector('.loader'),
+    btnSubmit: document.getElementById('OK'),
+    formWrapper: document.querySelector('.form-wrapper'),
+    exec: function() {
+        this.handleLoader();
+        this.registerEvent();
+    },
+    verify: function() {
+        if(this.username.value === '') {
+            alert('Please input your name!');
+        } else if(this.password.value === '') {
+            alert('Please input gift code!');
+        } else if(this.password.value !== 'tien') {
+            alert('Wrong gift code!');
+        } else {
+            alert('Success');
+        }
+        
+    },
+    registerEvent: function() {
+        this.formWrapper.addEventListener('keyup', (e) => {
+            e.preventDefault();
+            if(e.keyCode === 13) {
+                this.btnSubmit.click();
+            }
+        });
+        this.btnSubmit.addEventListener('click', () => {
+            this.verify();
+        })
+    },
+    handleLoader: function() {
+        window.addEventListener('load', () => {
+            setTimeout(() => {
+                this.loader.classList.add('is-hidden');
+            }, 2000);
+        });
+    }
 }
+
+VETRUONG3.exec.bind(VETRUONG3)();
