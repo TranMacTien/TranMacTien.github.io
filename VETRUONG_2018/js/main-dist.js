b35ca350fb2ff2bf1a9f268f7bca9a4b2ab635a2
mactien <mactien.tran@ogilvy.com>
2018-02-04 20:23:48 +0700

    update login page

--- VETRUONG_2018/js/main-dist.js
+++ VETRUONG_2018/js/main-dist.js
@@ -1,18 +1,29 @@
 "use strict";
 
 var VETRUONG3 = {
+    popup: {},
     username: document.getElementById("username"),
     password: document.getElementById("password"),
     loader: document.querySelector(".loader"),
     btnSubmit: document.getElementById("OK"),
     formWrapper: document.querySelector(".form-wrapper"),
+    container: document.querySelector(".container"),
     exec: function() {
-        this.registerEvent(), this.giftScreenControl();
+        this.handleLoader(), this.registerEvent();
     },
     verify: function() {
         if ("" === this.username.value) alert("Please input your name!"); else if ("" === this.password.value) alert("Please input gift code!"); else {
-            -1727222872 === this.hashPassword(this.password.value) ? alert("success") : alert("wrong!");
+            -1727222872 === this.hashPassword(this.password.value) ? this.loginSuccess() : this.loginFail();
         }
+    },
+    loginSuccess: function() {
+        this.initGiftScreen(), this.removeLoginPage();
+    },
+    loginFail: function() {
+        this.initPopup(), this.popup.showLoginFail();
+    },
+    removeLoginPage: function() {
+        document.querySelector(".cover-img").style.display = "none";
     },
     hashPassword: function(t) {
         for (var e = 0, n = 0; n < t.length; n++) {
@@ -37,8 +48,9 @@ var VETRUONG3 = {
         });
     },
     initGiftScreen: function() {
-        this.congratulation = document.createElement("div"), document.body.appendChild(this.congratulation), 
-        this.congratulation.className = "congratulation", this.congratulation.innerHTML = '<div class="congratulation">\n                                            <div class="congratulation__text">\n                                                <div class="congratulation__img-wrapper">\n                                                    <img class="congratulation__image bounceInDown" src="img/congratulation.png" alt="text">\n                                                </div>\n                                                <span class="congratulation__arrow">Scroll down</span>\n                                            </div>\n                                            <div class="row">\n                                                <img class="gift__image" src="img/ly_su.jpg" alt="gift">\n                                                <img class="gift__image" src="img/moc_khoa.jpg" alt="gift">\n                                                <img class="gift__image" src="img/ao_thun.jpg" alt="gift">\n                                            </div>\n                                        </div>';
+        this.congratulation = document.createElement("div"), this.container.appendChild(this.congratulation), 
+        this.congratulation.className = "congratulation", this.congratulation.innerHTML = '<div class="congratulation">\n                                            <div class="congratulation__text">\n                                                <div class="congratulation__img-wrapper">\n                                                    <img class="congratulation__image bounceInDown" src="img/congratulation.png" alt="text">\n                                                </div>\n                                                <span class="congratulation__arrow">Scroll down</span>\n                                            </div>\n                                            <div class="row gift">\n                                                <div class="gift__wrapper">\n                                                    <img class="gift__image" src="img/ly_su.jpg" alt="gift">\n                                                </div>\n                                                <div class="gift__wrapper">\n                                                    <img class="gift__image" src="img/moc_khoa.jpg" alt="gift">\n                                                </div>\n                                                <div class="gift__wrapper">\n                                                    <img class="gift__image" src="img/ao_thun.jpg" alt="gift">\n                                                </div>\n                                            </div>\n                                        </div>', 
+        this.giftScreenControl();
     },
     giftScreenControl: function() {
         window.addEventListener("scroll", function() {
@@ -47,6 +59,22 @@ var VETRUONG3 = {
                 t.classList.add("fade-in");
             });
         });
+    },
+    initPopup: function() {
+        var t = this;
+        this.popup.overlayBg = document.createElement("div"), this.popup.overlayBg.className = "overlay-bg", 
+        this.popup.popupContent = document.createElement("div"), this.popup.popupContent.className = "popup__content", 
+        this.popup.popupButton = document.createElement("button"), this.popup.popupButton.className = "popup__button", 
+        this.popup.popupButton.innerText = "OK", this.container.appendChild(this.popup.overlayBg), 
+        this.popup.overlayBg.appendChild(this.popup.popupContent), this.popup.showLoginFail = function() {
+            t.popup.popupContent.innerHTML = "Wrong password! </br>Please try again.", t.popup.popupContent.appendChild(t.popup.popupButton);
+        }, this.popup.eventClose = function() {
+            t.popup.popupButton.addEventListener("click", function() {
+                t.popup.close();
+            });
+        }, this.popup.close = function() {
+            t.popup.overlayBg.parentNode.removeChild(t.popup.overlayBg);
+        }, this.popup.eventClose();
     }
 };
 
--- VETRUONG_2018/js/main.js
+++ VETRUONG_2018/js/main.js
@@ -1,13 +1,15 @@
 let VETRUONG3 = {
+    popup: {},
     username: document.getElementById('username'),
     password: document.getElementById('password'),
     loader: document.querySelector('.loader'),
     btnSubmit: document.getElementById('OK'),
     formWrapper: document.querySelector('.form-wrapper'),
+    container: document.querySelector('.container'),
     exec: function () {
-        // this.handleLoader();
+        this.handleLoader();
         this.registerEvent();
-        this.giftScreenControl();
+
     },
     verify: function () {
         if (this.username.value === '') {
@@ -16,14 +18,28 @@ let VETRUONG3 = {
             alert('Please input gift code!');
         } else {
             let hashPass = this.hashPassword(this.password.value);
-            if(hashPass === -1727222872) {
-                alert('success');
+            if (hashPass === -1727222872) {
+               this.loginSuccess();
             } else {
-                alert('wrong!');
+               this.loginFail();
             }
         }
-
     },
+
+    loginSuccess: function() {
+        this.initGiftScreen();
+        this.removeLoginPage();
+    },
+
+    loginFail: function () {
+        this.initPopup();
+        this.popup.showLoginFail();
+    },
+
+    removeLoginPage: function() {
+        document.querySelector('.cover-img').style.display = 'none';
+    },
+
     hashPassword: function (password) {
         let hash = 0;
         for (let i = 0; i < password.length; i++) {
@@ -51,9 +67,9 @@ let VETRUONG3 = {
             }, 2000);
         });
     },
-    initGiftScreen: function() {
+    initGiftScreen: function () {
         this.congratulation = document.createElement('div');
-        document.body.appendChild(this.congratulation);
+        this.container.appendChild(this.congratulation);
         this.congratulation.className = 'congratulation';
         this.congratulation.innerHTML = `<div class="congratulation">
                                             <div class="congratulation__text">
@@ -62,29 +78,61 @@ let VETRUONG3 = {
                                                 </div>
                                                 <span class="congratulation__arrow">Scroll down</span>
                                             </div>
-                                            <div class="row">
-                                                <img class="gift__image" src="img/ly_su.jpg" alt="gift">
-                                                <img class="gift__image" src="img/moc_khoa.jpg" alt="gift">
-                                                <img class="gift__image" src="img/ao_thun.jpg" alt="gift">
+                                            <div class="row gift">
+                                                <div class="gift__wrapper">
+                                                    <img class="gift__image" src="img/ly_su.jpg" alt="gift">
+                                                </div>
+                                                <div class="gift__wrapper">
+                                                    <img class="gift__image" src="img/moc_khoa.jpg" alt="gift">
+                                                </div>
+                                                <div class="gift__wrapper">
+                                                    <img class="gift__image" src="img/ao_thun.jpg" alt="gift">
+                                                </div>
                                             </div>
                                         </div>`;
+        this.giftScreenControl();
     },
-    giftScreenControl: function() {
+    giftScreenControl: function () {
         window.addEventListener('scroll', () => {
             let scrollTop = document.body.scrollTop || document.documentElement.scrollTop;
             let anchor = document.querySelector('.congratulation__text').clientHeight / 3;
-            if(scrollTop > anchor) {
+            if (scrollTop > anchor) {
                 Array.from(document.querySelectorAll('.gift__image')).forEach((item, i) => {
-                    if(i === 1) {
+                    if (i === 1) {
                         item.style.transitionDelay = '.3s';
                     }
-                    if(i === 2) {
+                    if (i === 2) {
                         item.style.transitionDelay = '.6s';
                     }
                     item.classList.add('fade-in');
                 });
             }
         });
+    },
+    initPopup: function () {
+        this.popup.overlayBg = document.createElement('div');
+        this.popup.overlayBg.className = 'overlay-bg';
+        this.popup.popupContent = document.createElement('div');
+        this.popup.popupContent.className = 'popup__content';
+        this.popup.popupButton = document.createElement('button');
+        this.popup.popupButton.className = 'popup__button';
+        this.popup.popupButton.innerText = 'OK';
+        this.container.appendChild(this.popup.overlayBg);
+        this.popup.overlayBg.appendChild(this.popup.popupContent);
+        this.popup.showLoginFail = () => {
+            this.popup.popupContent.innerHTML = 'Wrong password! </br>Please try again.';
+            this.popup.popupContent.appendChild(this.popup.popupButton);
+        }
+        this.popup.eventClose = () => {
+            this.popup.popupButton.addEventListener('click', () => {
+                this.popup.close();
+            });
+        }
+        this.popup.close = () => {
+            this.popup.overlayBg.parentNode.removeChild(this.popup.overlayBg);
+        }
+        // exec func
+        this.popup.eventClose();
     }
 }
 
